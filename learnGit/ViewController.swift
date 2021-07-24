f5772684148c6197b6e660a64d78effd325cfb46
tran dinh thong <petter20062010@gmail.com>
2021-07-24 13:11:54 +0700

    init

---
+++ learnGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  learnGit
+//
+//  Created by tran dinh thong on 7/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
