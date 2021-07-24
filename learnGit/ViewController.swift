8be89018e0ed310726f496fac710224072651570
tran dinh thong <petter20062010@gmail.com>
2021-07-24 13:20:15 +0700

    test

--- learnGit/ViewController.swift
+++ learnGit/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("123---")
         // Do any additional setup after loading the view.
     }
 
